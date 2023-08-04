0cc9fba1b026150d4a5b40c77acb039e63595a92
spodder123 <120617408+spodder123@users.noreply.github.com>
2023-08-03 21:06:20 -0500

    Added footer file to be used on all files.

---
+++ footer.php
@@ -0,0 +1,23 @@
+
+<footer class="bg-dark text-light">
+    <hr>
+    <div class="container">
+        <div class="row d-flex" >
+            <div class="col-4">
+                <ul class="list-inline">
+                    <li class="list-inline-item "><a href="index.php" class="link-info text-decoration-none">Home</a></li>
+                    <li class="list-inline-item"><a href="signup.php" class="link-info text-decoration-none">Signup</a></li>
+                    <li class="list-inline-item"><a href="aboutUs.php" class="link-info text-decoration-none">About Us</a></li>
+                </ul>
+            </div>
+            <div class="col-4 d-flex justify-content-center ">
+                <p>Frendz Sports</p>
+            </div>
+            <div class="col-4 d-flex justify-content-end">
+                <p>&copy; 2023 FS. All rights reserved.</p>
+            </div>
+
+        </div>
+    </div>
+</footer>
+
