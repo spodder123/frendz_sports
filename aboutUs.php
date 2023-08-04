1028f88ea356eb1a0344c8b69aa264a41c6781ed
spodder123 <120617408+spodder123@users.noreply.github.com>
2023-08-03 21:20:54 -0500

    About us page added for the store description.

---
+++ aboutUs.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div class="content">
+<?php include('header.php'); ?>
+
+<div class="container mb-5">
+  <h1 class="text-center">About Us</h1>
+  <p>
+    Welcome to Frendz Sports, the premier destination for high-quality soccer equipment in the region. As a leading provider, we take immense pride in offering an extensive range of top-notch products designed to elevate your soccer experience. Whether you're a seasoned player or just starting your soccer journey, our diverse selection caters to all skill levels and preferences.
+  </p>
+  <p>
+    At Frendz Sports, our mission is to go above and beyond in providing you with exceptional products and customer service. We understand that soccer is not just a sport; it's a passion and a way of life for many. That's why we strive to bring you the best, handpicked soccer jerseys, crafted with superior materials for ultimate comfort and style.
+  </p>
+  <p>
+    Take a step towards optimizing your performance on the field with our carefully curated collection of football boots. From renowned brands to cutting-edge technologies, we have the ideal pair to match your playing style and enhance your skills.
+  </p>
+  <p>
+    For those fast-paced indoor matches, our indoor football shoes are specially designed to deliver maximum grip and maneuverability. Tackle the game with confidence, knowing that your footwear is tailored to perform on any indoor surface.
+  </p>
+  <p>
+    To complete your soccer gear, explore our range of accessories, from goalkeeper gloves to shin guards. Each accessory is chosen to complement your playing needs and provide you with the utmost protection and confidence on the pitch.
+  </p>
+  <p>
+    Our dedication to excellence has resulted in a loyal customer base that trusts us to deliver nothing but the best. We take your soccer journey seriously and continually update our product range to stay ahead of the game, incorporating the latest advancements in the soccer world.
+  </p>
+  <p>
+    Thank you for choosing Frendz Sports as your go-to destination for soccer accessories. It is an honor to be a part of your soccer adventure and to help you elevate your game to new heights. Our team of passionate soccer enthusiasts is always here to assist you in finding the perfect gear and ensuring you have a memorable shopping experience.
+  </p>
+  <p>
+    Let's embark on this exciting journey together, as you gear up with confidence and determination, knowing that Frendz Sports is committed to empowering you on your path to soccer greatness. See you on the field!
+  </p>
+</div>
+
+<?php include('footer.php'); ?>
+
+</body>
+</html>
+
+
